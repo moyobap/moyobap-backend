c7998fc42643accfaa1ecccb725171d5716e7d33
seungin <tmddls91@inha.edu>
2025-09-01 17:43:44 +0900

    [feat] Auth 토큰 응답 관련 dto 기본 구현

---
+++ src/main/java/com/moyobab/server/auth/dto/TokenResponse.java
@@ -0,0 +1,13 @@
+package com.moyobab.server.auth.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+}
