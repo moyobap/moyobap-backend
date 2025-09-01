7125c7bc64b0d7267b5b62d61a11e4c9a31b30e2
seungin <tmddls91@inha.edu>
2025-09-01 16:30:51 +0900

    [feat] User 관련 controller,repository,service 기본 틀 구성

---
+++ src/main/java/com/moyobab/server/user/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.moyobab.server.user.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+}
---
+++ src/main/java/com/moyobab/server/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.moyobab.server.user.repository;
+
+import com.moyobab.server.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
