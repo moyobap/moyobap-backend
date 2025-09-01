33d636f840f3795632f1200aefc208eff8fb382f
seungin <tmddls91@inha.edu>
2025-09-01 18:11:22 +0900

    [refactor] 역직렬화 전용 처리 및 입력값 검증 추가

--- src/main/java/com/moyobab/server/user/dto/UserLoginRequestDto.java
+++ src/main/java/com/moyobab/server/user/dto/UserLoginRequestDto.java
@@ -1,5 +1,9 @@
 package com.moyobab.server.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -7,6 +11,13 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginRequestDto {
+    @NotBlank
+    @Email
+    @Size(max = 320)
     private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 72)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 }
