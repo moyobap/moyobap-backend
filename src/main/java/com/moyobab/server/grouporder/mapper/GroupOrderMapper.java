6b1c1b8ad703e5d8a98a8258f342db1d73b35836
seungin <tmddls91@inha.edu>
2025-09-02 16:17:00 +0900

    [feat] 그룹주문 관련 mapper 초기 구현

---
+++ src/main/java/com/moyobab/server/grouporder/mapper/GroupOrderMapper.java
@@ -0,0 +1,24 @@
+package com.moyobab.server.grouporder.mapper;
+
+import com.moyobab.server.grouporder.dto.GroupOrderResponseDto;
+import com.moyobab.server.grouporder.entity.GroupOrder;
+
+import java.time.format.DateTimeFormatter;
+
+public class GroupOrderMapper {
+
+    // 날짜,시간 포멧
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static GroupOrderResponseDto toResponse(GroupOrder groupOrder) {
+        return GroupOrderResponseDto.builder()
+                .id(groupOrder.getId())
+                .menuCategory(groupOrder.getMenuCategory())
+                .brandName(groupOrder.getBrandName())
+                .expectedAmount(groupOrder.getExpectedAmount())
+                .maxDistance(groupOrder.getMaxDistance())
+                .deadlineTime(groupOrder.getDeadlineTime().format(formatter))
+                .isClosed(groupOrder.isClosed())
+                .build();
+    }
+}
