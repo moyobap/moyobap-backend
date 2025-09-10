50d9bddda427057c295abce6e2066c068809739e
seungin <tmddls91@inha.edu>
2025-09-10 16:45:03 +0900

    [refactor] 소셜로그인 대응으로 레포지토리 메서드 수정 및 Auth ErrorCase 추가

--- src/main/java/com/moyobab/server/user/repository/UserRepository.java
+++ src/main/java/com/moyobab/server/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.moyobab.server.user.repository;
 
+import com.moyobab.server.user.entity.LoginType;
 import com.moyobab.server.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndLoginType(String email, LoginType loginType);
 }
