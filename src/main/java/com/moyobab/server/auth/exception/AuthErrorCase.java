a34513f9d0197aa63ba09bb999df2059eb10b98e
seungin <tmddls91@inha.edu>
2025-09-10 16:36:30 +0900

    [refactor] auth 관련 ErrorCase 추가

--- src/main/java/com/moyobab/server/auth/exception/AuthErrorCase.java
+++ src/main/java/com/moyobab/server/auth/exception/AuthErrorCase.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorCase implements ErrorCase {
-    UNAUTHORIZED(401, 7001, "인증 정보가 없거나 userId를 추출할 수 없습니다.");
+    UNAUTHORIZED(401, 7001, "인증 정보가 없거나 userId를 추출할 수 없습니다."),
+    INVALID_REFRESH_TOKEN(401, 7002, "유효하지 않은 리프레시 토큰입니다."),
+    EXPIRED_REFRESH_TOKEN(401, 7003, "리프레시 토큰이 만료되었습니다.");
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
