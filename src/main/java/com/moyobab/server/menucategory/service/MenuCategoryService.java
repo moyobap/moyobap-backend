ba38dce1240c797d835f2bdeefd87af00aa85e5b
seungin <tmddls91@inha.edu>
2025-09-19 17:54:49 +0900

    [feat] 메뉴 카테고리 조회 관련 서비스 로직 구현

--- src/main/java/com/moyobab/server/menucategory/service/MenuCategoryService.java
+++ src/main/java/com/moyobab/server/menucategory/service/MenuCategoryService.java
@@ -1,7 +1,18 @@
 package com.moyobab.server.menucategory.service;
 
+import com.moyobab.server.menucategory.dto.MenuCategoryResponseDto;
+import com.moyobab.server.menucategory.entity.MenuCategoryType;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MenuCategoryService {
+
+    public List<MenuCategoryResponseDto> getAllCategories() {
+        return List.of(MenuCategoryType.values()).stream()
+                .map(MenuCategoryResponseDto::from)
+                .collect(Collectors.toList());
+    }
 }
