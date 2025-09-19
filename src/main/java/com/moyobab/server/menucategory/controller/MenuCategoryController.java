2ea36880330846bd4bdf3bfe8821aa4d32d0d05d
seungin <tmddls91@inha.edu>
2025-09-19 17:55:26 +0900

    [feat] 메뉴 카테고리 조회 관련 컨트롤러 로직 구현

--- src/main/java/com/moyobab/server/menucategory/controller/MenuCategoryController.java
+++ src/main/java/com/moyobab/server/menucategory/controller/MenuCategoryController.java
@@ -1,9 +1,31 @@
 package com.moyobab.server.menucategory.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.moyobab.server.global.response.CommonResponse;
+import com.moyobab.server.menucategory.dto.MenuCategoryResponseDto;
+import com.moyobab.server.menucategory.service.MenuCategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/menu-categories")
 public class MenuCategoryController {
+
+    private final MenuCategoryService menuCategoryService;
+
+    @GetMapping
+    @Operation(summary = "메뉴 카테고리 조회", description = "모든 메뉴 카테고리를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class)))
+    })
+    public CommonResponse<List<MenuCategoryResponseDto>> getCategories() {
+        return CommonResponse.success(menuCategoryService.getAllCategories());
+    }
 }
