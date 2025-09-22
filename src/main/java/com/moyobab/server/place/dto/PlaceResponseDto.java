c6424b13f5c23515039a8fb910f077db54fda179
seungin <tmddls91@inha.edu>
2025-09-23 01:51:10 +0900

    [feat] response 장소 Dto 구현

--- src/main/java/com/moyobab/server/place/dto/PlaceResponseDto.java
+++ src/main/java/com/moyobab/server/place/dto/PlaceResponseDto.java
@@ -1,4 +1,15 @@
 package com.moyobab.server.place.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PlaceResponseDto {
+    private String placeName;
+    private String addressName;
+    private String roadAddressName;
+    private String phone;
+    private double latitude;
+    private double longitude;
 }
