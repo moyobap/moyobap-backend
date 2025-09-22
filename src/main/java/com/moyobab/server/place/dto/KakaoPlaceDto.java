c9d360acd018b70abc8786896daf1f13d4814282
seungin <tmddls91@inha.edu>
2025-09-23 01:50:04 +0900

    [feat] 카카오 장소 관련 dto 구현

--- src/main/java/com/moyobab/server/place/dto/KakaoPlaceDto.java
+++ src/main/java/com/moyobab/server/place/dto/KakaoPlaceDto.java
@@ -1,4 +1,26 @@
 package com.moyobab.server.place.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+
+@Data
 public class KakaoPlaceDto {
+    @JsonProperty("place_name")
+    private String placeName;
+
+    @JsonProperty("address_name")
+    private String addressName;
+
+    @JsonProperty("road_address_name")
+    private String roadAddressName;
+
+    @JsonProperty("phone")
+    private String phone;
+
+    @JsonProperty("x")
+    private String longitude;
+
+    @JsonProperty("y")
+    private String latitude;
 }
