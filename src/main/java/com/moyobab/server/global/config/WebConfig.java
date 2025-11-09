a61d98aa6268430d4fcac93cb7b0b27b28a665c7
seungin <tmddls91@inha.edu>
2025-11-10 04:10:51 +0900

    [refactor] WebConfig에 커스텀 유저 리졸버 등록

--- src/main/java/com/moyobab/server/global/config/WebConfig.java
+++ src/main/java/com/moyobab/server/global/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.moyobab.server.global.config;
 
+import com.moyobab.server.auth.resolver.CurrentUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final CurrentUserArgumentResolver currentUserArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -14,5 +21,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(currentUserArgumentResolver);
     }
 }
