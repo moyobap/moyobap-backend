9ec26ee92094260e80c18a232f33586d0d0bff3f
seungin <tmddls91@inha.edu>
2025-11-23 13:57:21 +0900

    [refactor] 그룹 생성 관련해서 참여자 및 생성자 닉네임 받아오도록 로직 개선

--- src/main/java/com/moyobab/server/grouporder/mapper/GroupOrderMapper.java
+++ src/main/java/com/moyobab/server/grouporder/mapper/GroupOrderMapper.java
@@ -19,6 +19,8 @@ public class GroupOrderMapper {
                 .maxDistance(groupOrder.getMaxDistance())
                 .deadlineTime(groupOrder.getDeadlineTime().format(formatter))
                 .closed(groupOrder.isClosed())
+                .currentOrderAmount(0)
+                .creatorNickname(groupOrder.getCreator().getNickname())
                 .build();
     }
 }
