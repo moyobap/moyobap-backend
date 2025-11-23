aa4818d4ba6c667f8eeaebfa12be38a44245483e
seungin <tmddls91@inha.edu>
2025-11-23 15:04:07 +0900

    [fix] JOIN FETCH로 Lazy 로딩 문제 해결

--- src/main/java/com/moyobab/server/grouporder/repository/GroupOrderRepository.java
+++ src/main/java/com/moyobab/server/grouporder/repository/GroupOrderRepository.java
@@ -1,10 +1,12 @@
 package com.moyobab.server.grouporder.repository;
 
 import com.moyobab.server.grouporder.entity.GroupOrder;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface GroupOrderRepository extends JpaRepository<GroupOrder, Long> {
+    @EntityGraph(attributePaths = {"participants", "creator"})
     List<GroupOrder> findAllByClosedFalseOrderByDeadlineTimeAsc();  // 모집 중 + 마감 임박순 정렬
 }
